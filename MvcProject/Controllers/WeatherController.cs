3be284d18b2ccffdcf3b4b9ac395109c16523d95
radi <radoslav_galabov89@abv.bg>
2013-05-18 15:19:11 +0300

    My first commit with the whole project

---
+++ MvcProject/Controllers/WeatherController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcProject.Controllers;
+
+namespace MvcProject.Controllers
+{
+    public class WeatherController : Controller
+    {
+        //
+        // GET: /Weather/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Used to render the second window in the view
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        
+       
+    }
+}
